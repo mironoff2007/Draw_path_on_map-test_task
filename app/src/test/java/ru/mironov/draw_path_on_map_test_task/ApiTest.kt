5ea6344f5da807e1985d4cacafb220d22327d704
mironoff2007 <mironoff2007@rambler.ru>
2021-11-25 21:05:37 +0300

    parse GeoJson object, get polygon coordinates

--- app/src/test/java/ru/mironov/draw_path_on_map_test_task/ApiTest.kt
+++ app/src/test/java/ru/mironov/draw_path_on_map_test_task/ApiTest.kt
@@ -1,6 +1,11 @@
 package ru.mironov.draw_path_on_map_test_task
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
 import org.junit.Test
 import retrofit2.Call
 import retrofit2.Response
@@ -10,6 +15,12 @@ import ru.mironov.draw_path_on_map_test_task.NetworkService
 class ApiTest {
 
     var jo:JsonObject?=null
+    var f:JsonObject?=null
+    var g:JsonObject?=null
+   var polygons=arrayListOf<JsonArray?>()
+    var polygon=arrayListOf<JsonArray?>()
+
+
     @Test
     fun getRequestTest() {
 
@@ -18,7 +29,30 @@ class ApiTest {
         jo=response.body()
 
         val s= jo?.get("type")?.asString
-        assertEquals(true, s == "FeatureCollection")
+        f= jo?.get("features")?.asJsonArray?.get(0) as JsonObject?
+        g= f?.get("geometry") as JsonObject?
+        val c=g?.get("coordinates")?.asJsonArray
+
+        var i=0
+        var sizeCoords=0
+        if(c?.size()!=null){sizeCoords=c?.size()}
+        while(i<sizeCoords){
+            var p=c?.get(i) as JsonArray?
+            p=p?.get(0) as JsonArray?
+            polygons.add(p)
+            i++
+
+            var j=0
+            var sizePoligons=0
+            if(p?.size()!=null){sizePoligons=p?.size()}
+            while(j<sizePoligons){
+                val poly=p?.get(j) as JsonArray?
+                j++
+                System.out.println(poly?.get(0).toString()+"/"+poly?.get(1).toString())
+            }
+
+        }
+
     }
 
 }
