95fcb8ef92b44d96f6e8fdf5fb067e8aa20dfe8b
Mironov Ury <mironoff2007@rambler.ru>
2021-11-25 13:29:19 +0300

    Get field from json

--- app/src/test/java/ru/mironov/draw_path_on_map_test_task/ApiTest.kt
+++ app/src/test/java/ru/mironov/draw_path_on_map_test_task/ApiTest.kt
@@ -9,13 +9,16 @@ import ru.mironov.draw_path_on_map_test_task.NetworkService
 
 class ApiTest {
 
+    var jo:JsonObject?=null
     @Test
     fun getRequestTest() {
 
         val  call:Call<JsonObject>? = NetworkService.getJSONApi().getCurrencyMap()
         val response: Response<JsonObject> =call!!.execute()
-        var b=response.body()
-        assertEquals(true, true)
+        jo=response.body()
+
+        val s= jo?.get("type")?.asString
+        assertEquals(true, s == "FeatureCollection")
     }
 
 }
